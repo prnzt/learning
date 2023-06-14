65886758ce835f3dc964980aca51575588b55444
Shobhit Pal <shobhit.pal@siemens-energy.com>
2023-06-15 00:51:15 +0530

    Added Employee domain class

---
+++ EmployeeManagement/src/main/java/com/learning/domain/Employee.java
@@ -0,0 +1,5 @@
+package com.learning.domain;
+
+public class Employee {
+
+}
--- EmployeeManagement/src/main/java/com/learning/ems/main/EmployeeManagementApplication.java
+++ EmployeeManagement/src/main/java/com/learning/ems/main/EmployeeManagementApplication.java
@@ -1,4 +1,4 @@
-package com.learning.employee;
+package com.learning.ems.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
