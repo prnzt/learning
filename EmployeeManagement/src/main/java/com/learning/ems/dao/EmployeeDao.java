b480a41b16fc69c334916b4468516be88c4bfcd6
Nehe Pal <nehapal2209@gmail.com>
2023-06-19 01:38:50 +0530

    Added POST UPDATE and DELETE Crud operations

--- EmployeeManagement/src/main/java/com/learning/ems/dao/EmployeeDao.java
+++ EmployeeManagement/src/main/java/com/learning/ems/dao/EmployeeDao.java
@@ -18,5 +18,28 @@ public class EmployeeDao {
 	{
 		return stmt.query("select * from employee", new BeanPropertyRowMapper<Employee>(Employee.class));
 	}
+
+	public Employee getEmployeebyId(int id) 
+	{
+		
+		return stmt.queryForObject("select * from employee where employeeid=?",new Object[] {id}, new BeanPropertyRowMapper<Employee>(Employee.class));
+	}
+	
+	public int saveEmployee(Employee employee)
+	{
+		
+		return stmt.update("insert into employee (employeeid, employeename, employeedept, employeedoj, employeedob, employeerole, employeeaddress, employeesalary) values (?,?,?,?,?,?,?,?)",employee.getEmployeeId(),employee.getEmployeeName(),
+				employee.getEmployeeDept(),employee.getEmployeeDOJ(),employee.getEmployeeDOB(),employee.getEmployeeRole(),employee.getEmployeeAddress(),employee.getEmployeeSalary());
+	}
+	
+	public int deleteEmployee(int id)
+	{
+		return stmt.update("delete from employee where employeeid=?" ,id);
+	}
+	
+	public int updateEmployee(Employee employee)
+	{
+		return stmt.update("update employee set employeesalary=? , employeedob=? where employeeid=?" , employee.getEmployeeSalary(),employee.getEmployeeDOB(),employee.getEmployeeId());
+	}
 	
 }
--- EmployeeManagement/src/main/java/com/learning/ems/service/EmployeeService.java
+++ EmployeeManagement/src/main/java/com/learning/ems/service/EmployeeService.java
@@ -7,5 +7,9 @@ import com.learning.ems.domain.Employee;
 public interface EmployeeService {
 
 	public List<Employee> getAllEmployeeDetails();
+	public Employee getEmployeebyID(int id);
+	public int saveEmployee(Employee employee);
+	public int deleteEmployee(int id);
+	public int updateEmployee(Employee employee);
 	
 }
